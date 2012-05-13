0bb8c2d088edd55db998741f8d03b53e2beac536
Davis Zanetti Cabral <daviscabral@gmail.com>
2012-05-12 22:27:53 -0300

    Separado a parte do cenário do herói

--- Jogo.java
+++ Jogo.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 class Jogo {
 
 	public static void main(String args[]){
@@ -6,15 +7,15 @@ class Jogo {
 		Wumpus w = new Wumpus();
 		int direcao = -1;
 		// 0 - sair 8 - andar norte 2 - andar sul 4 - andar 6 - andar oeste 5 - atirar
-		while(direcao != 0 && w.morreu == false){
+		while(direcao != 0 && !w.getHeroi().estaVivo()){
 			System.out.print("\nFaça o seu movimento: ");
 			direcao = in.nextInt();
 			if(direcao == 5){
 				System.out.println("Atirar em qual direção?");
 				direcao = in.nextInt();
-				w.atirar(direcao);
+				w.getHeroi().atirar(direcao);
 			} else {
-				w.andar(direcao);
+				w.getHeroi().andar(direcao);
 			}
 		}
 		System.out.println("");
